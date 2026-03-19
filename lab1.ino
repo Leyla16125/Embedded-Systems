b3efad71fb3cf56a051dc6347ae09d80e8bfdac1
Leyla Huseynova <118552639+Leyla16125@users.noreply.github.com>
2026-03-19 06:08:32 +0400

    Update lab1.ino

--- lab1.ino
+++ lab1.ino
@@ -1,6 +1,7 @@
+// 1
 const int LED1 = 8;
 const int LED2 = 9;  
-int LED3 = 10;
+const int LED3 = 10;
 
 void setup() {
   pinMode(LED1, OUTPUT);
@@ -10,11 +11,24 @@ void setup() {
 
 void loop() {
   digitalWrite(LED1, HIGH); digitalWrite(LED2, LOW);  digitalWrite(LED3, LOW);
-  delay(250);
+  delay(300);
 
   digitalWrite(LED1, LOW);  digitalWrite(LED2, HIGH); digitalWrite(LED3, LOW);
-  delay(250);
+  delay(300);
 
   digitalWrite(LED1, LOW);  digitalWrite(LED2, LOW);  digitalWrite(LED3, HIGH);
-  delay(250);
+  delay(300);
+}
+
+
+//2
+void setup() {
+  DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2); //00000111
+}
+
+void loop() {
+  PORTB = (1 << PB0);//00000001
+  PORTB = (1 << PB1);//00000010
+  PORTB = (1 << PB2);//00000100
+  PORTB = 0x00;//00000000
 }
